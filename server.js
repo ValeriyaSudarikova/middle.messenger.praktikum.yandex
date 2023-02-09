44a249f4e017b9f8fb8fb3b307c7634c79b7deae
Valeriya Sudarikova <>
2023-02-09 11:07:00 +0400

    fix

--- server.js
+++ server.js
@@ -1,13 +1,10 @@
 const express = require('express');
 
 const app = express();
-const PORT = 4000;
+const PORT = 3000;
 
-app.use(express.static("./"));
+app.use('/static', express.static('public'));
 
-app.get('./', (req, res) => {
-    res.send('index.html')
-})
 app.listen(PORT, function () {
     console.log(`Example app listening on port ${PORT}!`);
 });
