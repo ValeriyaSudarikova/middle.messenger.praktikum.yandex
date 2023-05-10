a5b14cbc281021c6e2608bb2fd02350af4b6f4a6
Valeriya Sudarikova <>
2023-05-10 16:31:32 +0400

    fix

--- src/server.js
+++ src/server.js
@@ -3,10 +3,7 @@ const express = require("express")
 const app = express()
 const PORT = 3000
 
-// express.static(__dirname + '/dist')
-
 app.use("/", express.static("./dist"))
-
 
 app.listen(PORT, function () {
 	console.log(`Example app listening on port ${PORT}!`)
